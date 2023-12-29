678bb1419b6f21f704cb7891831f2b07438a6785
ahmed-fawzy-hub <af348565@gmail.com>
2023-12-29 20:05:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mydatabinding/BindingAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.mydatabinding
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter(value = ["myColors"])
+fun setMyColors(view: View, myColors: MyColors) {
+    when (myColors) {
+        MyColors.RED -> view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.red))
+        MyColors.YELLOW -> view.setBackgroundColor(
+            ContextCompat.getColor(
+                view.context,
+                R.color.yellow
+            )
+        )
+
+        MyColors.BLUE -> view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.blue))
+    }
+}
+@BindingAdapter(value = ["showIfTrue"])
+fun setCustomVisibility(view: View, value: Boolean?) {
+    if (value == true) {
+        view.visibility = View.VISIBLE
+    } else {
+        view.visibility = View.GONE
+    }
+
+}
---
+++ app/src/main/java/com/example/mydatabinding/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.mydatabinding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val loading=MutableLiveData<Boolean>(true)
+    fun stopLoading(){
+        loading.postValue(false)
+    }
+}
